4cd557a25f2e671a313e270ffe59ac80236fe025
msbicer <serdar@bicer.fake>
2014-01-14 12:04:29 +0200

    .

--- src/com/valven/kodio/KodIO.java
+++ src/com/valven/kodio/KodIO.java
@@ -7,6 +7,7 @@ import com.valven.kodio.util.ImageLoader;
 
 import android.app.Application;
 
+//VALVEN'de calismak/staj yapmak istiyorsan CV'ni ik [at] valven.com adresine ilani gordugun yeri belirterek gonder
 public class KodIO extends Application{
 	
 	public static JSONObject DATA = null;
